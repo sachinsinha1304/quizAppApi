8e83a1a3e4cfc4fa34b602dfd526d5ebca25ccf0
root <root@sachin>
2025-06-20 22:51:27 +0530

    adding docker file and docker compose

--- configs/db.js
+++ configs/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'sachin',
-  database: 'quiz'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'quiz'
 });
 
 module.exports = db;
